d7e5109b92704bbbadce8e6c9195fdde9e814968
Nick <nick@xtremecontrols.co.uk>
2023-12-29 12:53:09 +0000

    feat: only shows top three and for top time it allows for additional info

--- src/main.js
+++ src/main.js
@@ -1,7 +1,20 @@
 import { createApp } from "vue";
+import "vuetify/dist/vuetify.min.css";
+import { createVuetify } from "vuetify";
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
 import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
 
-createApp(App).use(store).use(router).mount("#app");
+const vuetify = createVuetify({
+    components,
+    directives,
+});
+
+createApp(App)
+    .use(store)
+    .use(vuetify)
+    .use(router)
+    .mount("#app");
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,94 +1,190 @@
 <template>
     <div class="app">
         <header class="app-header">
-            Countdown to New Year!
+            Upcoming New Year's Countdowns (Top Group with Details)
         </header>
-        <div class="countdown">
-            <div v-for="time in times" :key="time.label" class="time-section">
-                <span class="time-value">{{ time.value }}</span>
-                <span class="time-label">{{ time.label }}</span>
+        <div v-for="(group, index) in groupedTimezones" :key="group.timeUntilNewYear" class="grouped-timezone-countdown">
+            <!-- Display the first group with expansion panels for each timezone -->
+            <div v-if="index === 0">
+                <div class="top-group">
+                    {{ group.countdown.days }} days, {{ group.countdown.hours }} hours, {{ group.countdown.minutes }} minutes, {{ group.countdown.seconds }} seconds
+                </div>
+                <v-expansion-panels>
+                    <v-expansion-panel v-for="name in group.names" :key="name">
+                        <v-expansion-panel-title>{{ name }}</v-expansion-panel-title>
+                        <v-expansion-panel-text>
+                            <!-- Content for expandable menu; to be defined later -->
+                            More options and additional information for {{ name }}...
+                        </v-expansion-panel-text>
+                    </v-expansion-panel>
+                </v-expansion-panels>
+            </div>
+
+            <!-- Display remaining groups without expansion panels -->
+            <div v-else>
+                <div class="timezone-names">{{ group.names.join(', ') }}</div>
+                <div class="countdown-timer">
+                    {{ group.countdown.days }} days,
+                    {{ group.countdown.hours }} hours,
+                    {{ group.countdown.minutes }} minutes,
+                    {{ group.countdown.seconds }} seconds
+                </div>
             </div>
         </div>
     </div>
 </template>
 
 <script>
-import { ref, onMounted, onUnmounted } from "vue";
+import moment from "moment-timezone";
+import { ref, reactive, computed, onMounted, onUnmounted } from "vue";
 
 export default {
     name: "App",
     setup() {
-        const times = ref([
-            { label: "Days", value: 0 },
-            { label: "Hours", value: 0 },
-            { label: "Minutes", value: 0 },
-            { label: "Seconds", value: 0 },
-        ]);
+        const timezones = ref(moment.tz.names()); // all timezones
+        const countdowns = reactive({}); // reactive object to hold countdowns
 
-        function updateCountdown() {
-            const now = new Date();
-            const newYear = new Date(`January 1, ${now.getFullYear() + 1} 00:00:00`);
-            const diff = newYear - now;
+        // Calculate countdown for a specific timezone
+        function calculateCountdown(timezone) {
+            const newYear = moment.tz("2024-01-01 00:00", timezone);
+            const now = moment.tz(timezone);
+            const duration = moment.duration(newYear.diff(now));
 
-            // Calculate time components
-            times.value[0].value = Math.floor(diff / (1000 * 60 * 60 * 24)); // days
-            times.value[1].value = Math.floor((diff / (1000 * 60 * 60)) % 24); // hours
-            times.value[2].value = Math.floor((diff / 1000 / 60) % 60); // minutes
-            times.value[3].value = Math.floor((diff / 1000) % 60); // seconds
+            return {
+                days: duration.days(),
+                hours: duration.hours(),
+                minutes: duration.minutes(),
+                seconds: duration.seconds(),
+                timeUntilNewYear: newYear.diff(now), // total milliseconds until New Year
+            };
         }
+
+        // Initialize countdowns for all timezones
+        for (const zone of timezones.value) {
+            countdowns[zone] = calculateCountdown(zone);
+        }
+
+        // Group timezones by the countdown time
+        const groupedTimezones = computed(() => {
+            const groups = {};
+            for (const zone of timezones.value) {
+                const countdown = countdowns[zone];
+                // Create a unique key for the group
+                const timeKey = `${countdown.days}:${countdown.hours}:${countdown.minutes}:${countdown.seconds}`;
+                // Skip past New Year's events
+                // eslint-disable-next-line no-continue
+                if (countdown.timeUntilNewYear < 0) continue;
+                if (!groups[timeKey]) {
+                    groups[timeKey] = { names: [], countdown, timeUntilNewYear: countdown.timeUntilNewYear };
+                }
+                groups[timeKey].names.push(zone);
+            }
+            // Convert the groups object into an array, sort by time until New Year, and take the first 3
+            return Object.values(groups)
+                .sort((a, b) => a.timeUntilNewYear - b.timeUntilNewYear)
+                .slice(0, 3);
+        });
 
         // Update countdown every second
         let intervalId;
         onMounted(() => {
-            updateCountdown();
-            intervalId = setInterval(updateCountdown, 1000);
+            intervalId = setInterval(() => {
+                for (const zone of timezones.value) {
+                    countdowns[zone] = calculateCountdown(zone); // update each countdown
+                }
+            }, 1000);
         });
 
-        // Clear interval when component unmounts to prevent memory leaks
+        // Clear interval when component unmounts
         onUnmounted(() => {
             clearInterval(intervalId);
         });
 
-        return { times };
+        return { groupedTimezones };
     },
 };
 </script>
 
   <style>
-  /* Add your CSS styling here */
-  .app {
-  font-family: 'Arial', sans-serif;
-  text-align: center;
-  color: white;
-  background-color: #333; /* Dark background for contrast */
+/* General Styles */
+.app {
+  font-family: Arial, sans-serif;
+  color: #333;
+  max-width: 800px;
+  margin: 0 auto;
 }
 
 .app-header {
-  background-color: #444;
+  font-size: 2rem;
+  text-align: center;
+  margin-bottom: 20px;
+}
+
+/* Grouped Timezone Countdown Styles */
+.grouped-timezone-countdown {
+  border-bottom: 1px solid #ccc;
+  padding: 20px;
+  transition: background-color 0.3s;
+}
+
+.grouped-timezone-countdown:hover {
+  background-color: #f9f9f9;
+}
+
+/* First Group Emphasis */
+.grouped-timezone-countdown:first-child {
+  font-size: 1.5rem; /* Larger Font */
+  background-color: #e8e8e8; /* Different background */
+}
+
+/* Countdown Timer Styles */
+.countdown-timer {
+  font-size: 3rem;
+  font-weight: bold;
+  margin: 10px 0;
+}
+
+/* Timezone Names List */
+.timezone-names {
+  font-size: 1.2rem;
+  margin-bottom: 10px;
+  display: block; /* Makes it easier to style individual names later */
+}
+
+/* Expandable Menu Placeholder */
+.expandable-menu {
+  background-color: #f0f0f0;
+  border: 1px solid #ddd;
   padding: 10px;
-  font-size: 24px;
-  color: #ddd;
+  margin-top: 10px;
+  cursor: pointer;
+  user-select: none;
 }
 
-.countdown {
-  display: flex;
-  justify-content: center;
-  margin-top: 20px;
+.expandable-menu:not(:last-child) {
+  border-bottom: none;
 }
 
-.time-section {
-  margin: 0 10px;
+/* Hidden initially */
+.expandable-content {
+  display: none;
+  padding: 10px;
+  border-top: 1px solid #ddd;
 }
 
-.time-value {
-  font-size: 56px;
-  line-height: 1;
-}
-
-.time-label {
+/* Show content when .expandable-menu is clicked */
+.expandable-menu.active + .expandable-content {
   display: block;
-  font-size: 18px;
 }
 
-  /* Continue styling... */
+/* Interactive Items */
+.interactive-item {
+  cursor: pointer;
+  user-select: none;
+}
+
+.top-group {
+  font-size: 1.5rem; /* Larger font size for the top group */
+  margin-bottom: 20px;
+}
   </style>
